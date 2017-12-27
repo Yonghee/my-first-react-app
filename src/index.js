487ca783ad6efb95eec7b9d5689d44043b3b3093
nomad <goodyhlee@coupang.com>
2017-12-27 11:14:42 +0900

    complete!

--- src/index.js
+++ src/index.js
@@ -26,6 +26,9 @@ class Board extends React.Component {
 
     handleClick(i) {
         const squares = this.state.squares.slice();
+        if ( calulateWinner(squares) || squares[i]) {
+          return;
+        }
         squares[i] = this.state.xIsNext ? 'X' : 'O';
         this.setState({
           squares: squares,
@@ -34,7 +37,16 @@ class Board extends React.Component {
     }
 
     render() {
-        const status = 'Next player: ' + (this.state.xIsNext? 'X' : 'O');
+
+        const winner = calulateWinner(this.state.squares);
+        let status;
+
+        if (winner) {
+          status = 'Winnder: ' + winner;
+        } else {
+          status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
+        }
+
 
         return (
             <div>
@@ -73,6 +85,27 @@ class Game extends React.Component {
             </div>
         );
     }
+}
+
+function calulateWinner(squares) {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+
+  for(let i=0;i<lines.length;i++){
+      const[a,b,c] = lines[i];
+      if(squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+        return squares[a];
+      }
+  }
+  return null;
 }
 
 // ========================================
